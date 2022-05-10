603ef23d0a2285337ab8dac80b3818eea2f9d676
Igor Cristian <icdias@gmail.com>
2022-05-10 16:31:27 -0300

    Criação de um novo jogo, aula de flask.

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
+class Produto:
+    def __init__(self, nome, categoria, marca):
+        self.nome = nome
+        self.categoria = categoria
+        self.marca = marca
 
 app = Flask(__name__)
 
 @app.route("/inicio")
 def ola():
-    return "<h1>Olá mundo!</h1>"
+    produto1 = Produto('Builder Base', 'Gel', 'Unique Nail')
+    produto2 = Produto('Monomer', 'Acrílico', 'Psiu')
+    produto3 = Produto('Passo 5', 'Blindagem', 'Psiu')
+
+    lista = [produto1, produto2, produto3]
+    return render_template("lista.html", titulo="Unique Nail", produtos=lista)
 
 app.run()
